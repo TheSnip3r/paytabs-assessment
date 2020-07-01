ea0c762e65f8b0b00265973c05bebdb3daea6ef1
Abdelrahman <prog.a.bahnasy@gmail.com>
2020-07-01 03:23:47 +0200

    Added categories queries

--- application/models/Category.php
+++ application/models/Category.php
@@ -11,4 +11,16 @@ class Category extends CI_Model
 		parent::__construct();
 		$this->db = $this->load->database('default', TRUE);
 	}
+
+	public function getMainCategories()
+	{
+		$query = $this->db->select('id, name')->get_where('categories', array('id' => null));
+		return $query->result;
+	}
+
+	public function getChildren($parentID)
+	{
+		$query = $this->db->select('id, name')->get_where('categories', array('parent_id' => $parentID));
+		return $query->result;
+	}
 }
