e9fcb07ced7f35708fb5cf28510702adfee36596
Abdelrahman <prog.a.bahnasy@gmail.com>
2020-07-01 05:27:04 +0200

    Added migration class for creating categories table

---
+++ application/migrations/001_add_categories.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_categories extends CI_Migration {
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'INT',
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'title' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+			),
+			'parent_id' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+			),
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('categories');
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('categories');
+	}
+}
